919ec75a660a206e4edcbd76d3425dab94221702
root <root@ip-172-31-24-39.us-west-2.compute.internal>
2017-03-11 16:15:37 +0000

    일부 코드 이름 변경

--- GodDo/urls.py
+++ GodDo/urls.py
@@ -21,8 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-	url('', create),
+	url('', index),
 	url(r'^admin/', admin.site.urls),
-	url('hello', index),
 	url('create', create),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
